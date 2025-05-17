7252f7048ff5191031650eac78fd6ee4a3a3d0a4
jotaaave <marbenebela@gmail.com>
2025-05-17 19:47:01 -0300

    first commit

---
+++ src/container/home/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./style";
+
+export default function HomePage() {
+    return (
+        <Container>
+            <h1>Olá ANki</h1>
+        </Container>
+    );
+}
---
+++ src/style/theme.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export const theme = {
+    text: {
+        200: "#545454",
+        300: "#272626",
+        600: "#000000"
+    },
+    colors: {
+        purple: {
+            medium: "#2f3c87",
+        }
+    }
+};
